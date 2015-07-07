6651abfc43c01588a47524558041a77943c47f30
letterwind <letterwind0317@gmail.com>
2015-07-07 18:03:59 +0800

    測試拖拉事件

--- app/scripts/controllers/sort.js
+++ app/scripts/controllers/sort.js
@@ -17,9 +17,17 @@ angular.module('demoSortableApp')
      return sourceItemHandleScope.itemScope.sortableScope.$id !== destSortableScope.$id;
      },*/
     itemMoved: function (event) {
+      console.log('itemMoved',event);
       event.source.itemScope.modelValue.status = event.dest.sortableScope.$parent.column.name;
     },
     orderChanged: function (event) {
+      console.log('orderChanged',event);
+    },
+    dragStart:function(event){
+      console.log('dragStart',event);
+    },
+    dragEnd:function(event){
+      console.log('dragEnd',event);
     },
     containment: '#board'
   };
